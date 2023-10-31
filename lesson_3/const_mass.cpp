ca8b30a1e6cc947a25d7e2d55aa6e6d2205ec9b4
Ivan Ovsyukov <st117301@student.spbu.ru>
2023-10-31 11:37:15 +0300

    Two version code work with array. Work good.

--- lesson_3/const_mass.cpp
+++ lesson_3/const_mass.cpp
@@ -13,15 +13,15 @@ int main(){
     int a[n], b[n], c[n];
     for (int i=0; i<n; ++i){
         if (i>1){
-            b[i]=b[i]+i;
+            b[i]=b[i-1]+i;
         } else{
             b[i]=i;
         }
         a[i]=i;
         c[i]=i*i;
     }
-    printArray(a, n, "Count from 1 to n");
+    printArray(a, n, "Count from 0 to n-1");
     printArray(b, n, "Sum number a");
-    printArray(c, n, "square of number from 1 to n");
+    printArray(c, n, "Square of number from 0 to n-1");
     return 0;
 }
