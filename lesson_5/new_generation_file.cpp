70ac1f2a0341964e2492559748edb3b82406b033
Ivan Ovsyukov <st117301@student.spbu.ru>
2023-11-21 13:46:19 +0300

    new feneration file with buffer

---
+++ lesson_5/new_generation_file.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[]){
+    if (argc<3){
+        std::cout << "You have less argument" << std::endl;
+        return 1;
+    }
+    std::fstream fs;
+    fs.open("D:\\program\\Git\\Classwork-Practice_for-computer\\lesson_5\\file_generate_new.txt", std::ios_base::out | std::ios_base::binary);
+    if (!fs.is_open()){
+        std::cout << "File array.txt can not open." << std::endl;
+        return 1;
+    }
+    srand(0);
+    size_t filesize=atoi(argv[1]);
+    char buffer[atoi(argv[2])];
+    for (size_t k=0; k<filesize/sizeof(buffer); ++k){
+        for (size_t j=0; j<sizeof(buffer); ++j){
+            buffer[j]=static_cast<char>(rand());
+        }
+        fs.write(buffer, sizeof(buffer));
+    }
+    /*
+    int mass;
+    int randy;
+    std::cout << "Input count bites of generate file: ";
+    std::cin >> mass;
+    while (mass!=0){
+        randy=rand();
+        if ((sizeof(randy)*2)<=mass){
+            std::cout << sizeof(randy) << " " << mass << " " << randy << std::endl;
+            mass-=2*sizeof(randy);
+            std::cout << sizeof(randy) << " " << mass << std::endl;
+            fs << randy;
+        }
+    }
+    */
+    fs.close();
+    return 0;
+}
