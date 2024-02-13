f53ce8d9b9b83c61bbcd31e7980e27355e5f359c
Ivan Ovsyukov <st117301@student.spbu.ru>
2024-02-13 10:27:51 +0300

    create bmp template

---
+++ lesson_10/create_bmp_important_information.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+int main(){
+    std::ifstream one;
+    one.open("D:\\program\\Git\\Classwork-Practice_for-computer\\lesson_10\\pic_1.bmp", std::ios_base::in | std::ios_base::binary);
+    if (!one.is_open()){
+        std::cout << "one is not read" << std::endl;
+        return 1;
+    }
+    std::ofstream out;
+    out.open("D:\\program\\Git\\Classwork-Practice_for-computer\\lesson_10\\template.bmp", std::ios_base::out | std::ios_base::binary);
+    if (!out.is_open()){
+        one.close();
+        std::cout << "out is not read" << std::endl;
+        return 1;
+    }
+    char s;
+    for(int i=0; i<54; ++i){
+        one >> s;
+        out << s;
+    }
+    one.close();
+    out.close();
+    return 0;
+}
