9738286e1af11679b3ece5a05c342bfdd9807f29
Ivan Ovsyukov <st117301@student.spbu.ru>
2024-02-13 09:58:32 +0300

    test for work with files

--- lesson_10/work_with_bmp.cpp
+++ lesson_10/work_with_bmp.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <fstream>
 
 int main(){
+    std::ifstream one;
+    one.open("pic_1.bmp", std::ios_base::binary);
+    if (!one.is_open()){
+        std::cout << "one is not read" << std::endl;
+        return 1;
+    }
+    std::ifstream two;
+    two.open("pic_2.bmp", std::ios_base::binary);
+    if (!two.is_open()){
+        one.close();
+        std::cout << "two is not read" << std::endl;
+        return 1;
+    }
+    one.close();
+    two.close();
     return 0;
 }
